1ec25834773cbdb6e2577c99f68d6b3799aae57d
Gopi212006 <gopinathjdckap.com>
2025-03-22 11:29:16 +0530

    movie task

--- wether.js
+++ wether.js
@@ -1,19 +1,22 @@
+/** @format */
+
 let searchbtn = document.getElementById("searchbtn");
 
 let weather = async (city) => {
-    let weatherapi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8042b4f5b9580620c4a5aa4a8f086451&units=metric`;
+  let weatherapi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8042b4f5b9580620c4a5aa4a8f086451&units=metric`;
 
-    let weatherGet = await fetch(weatherapi);
-    let weatherOutput = await weatherGet.json();
+  let weatherGet = await fetch(weatherapi);
+  let weatherOutput = await weatherGet.json();
 
-    return weatherOutput;
+  return weatherOutput;
 };
-
-
 function showWeather(city) {
-    weather(city).then((data) => {
-        let maincontainer = document.getElementById("weather-info");
-        maincontainer.innerHTML = `
+  weather(city).then((data) => {
+    let maincontainer = document.getElementById("weather-info");
+    let icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    console.log(icon);
+    maincontainer.innerHTML = `
+             <div id="icon">  <img src="${icon}" alt=""></div>
             <div id="temp">${data.main.temp}°C</div>
             <div id="location">${data.name}</div>
             <div id="details">
@@ -22,17 +25,15 @@ function showWeather(city) {
                 <div>Description: ${data.weather[0].description}</div>
             </div>
         `;
-
-        console.log(data.weather);
-    });
+  });
 }
-
-
-
+showWeather("chennai");
 searchbtn.addEventListener("click", () => {
-    let place = document.getElementById("inputValue").value;
-    if (place) {
-        showWeather(place);
-    }
-    document.getElementById("inputValue").value = ""; 
+  let place = document.getElementById("inputValue").value;
+  if (place == "") {
+    alert("Enter any place");
+  } else {
+    showWeather(place);
+  }
+  document.getElementById("inputValue").value = "";
 });
